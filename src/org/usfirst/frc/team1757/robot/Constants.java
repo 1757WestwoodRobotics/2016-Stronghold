28a28f789a67cfa37922db9c3a06c7023da0aec7
Larry Tseng <lt222888@gmail.com>
2016-02-21 12:54:31 -0500

    Fixed Constants.java file

--- src/org/usfirst/frc/team1757/robot/Constants.java
+++ src/org/usfirst/frc/team1757/robot/Constants.java
@@ -5,11 +5,9 @@ public final class Constants {
 	private Constants() {
 		
 	}
-	
-	public static final int BUTTON_A = 2;
-	public static final int BUTTON_B = 3;
+
 	public static final int
-	BUTTON_X = 1,
+	BUTTON_X = 1, BUTTON_A = 2, BUTTON_B = 3,
 	BUTTON_Y = 4, BUTTON_LB = 5, BUTTON_RB = 6,
 	BUTTON_LT = 7, BUTTON_RT = 8,
 	BUTTON_BACK = 9, BUTTON_START = 10, BUTTON_LS = 11, BUTTON_RS = 12,
